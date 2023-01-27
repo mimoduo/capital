d27bc30e7150d78c7d04d49130c45c2af5ead340
Bryan Stoner <mimoduo@gmail.com>
2023-01-27 11:32:41 -0500

    New copy

--- src/twig/components/text-image-block/text-image-block.config.js
+++ src/twig/components/text-image-block/text-image-block.config.js
@@ -1,8 +1,6 @@
 module.exports = {
 	status: 'wip',
 	context: {
-		text: '<h2>What we do</h2><p>In sum, we bring private capital to infrastructure projects - mostly fiber optic cable routes. We also have invested in data center projects across the country - and are open to investments in “utilities” like ports. In sum, we bring private capital to infrastructure projects - </p><p>mostly fiber optic cable routes. We also have invested in data center projects across the country - and are open to investments in “utilities” like ports. In sum, we bring private capital to infrastructure project - mostly fiber optic cable and are open to investments in “utilities” like ports. In sum, we bring private capital to infrastructure project - mostly fiber optic cable.</p>',
+		text: '<h2>What we do</h2><p>Critical Infrastructure Partners (CIP) is a private investment company focused on telecommunication infrastructure with an emphasis on middle and long-haul dark fiber optic routes throughout North America. </p><p>We partner with best-in-class telecom providers and hyperscale users to provide best-in-class solutions to their clients and end users.</p>',
 	}
 };
-
-
